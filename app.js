fd5958ce7d77f63399aa43143aea74239b2d7b4b
Roberta Häggström <roberta.haggstrom@chasacademy.se>
2018-01-24 14:12:52 +0100

    initial structure with draggable and droppable

---
+++ app.js
@@ -0,0 +1,9 @@
+$(function() {
+    $(".list-cards, .list-container").draggable();
+    $(".list-cards").droppable();
+    //eventuellt någon effekt här
+});
+
+$(function() {
+    $("#date-picker").datepicker();
+});
